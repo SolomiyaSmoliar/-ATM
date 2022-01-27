cc931ea585c41a1cfb671df6751aa03455ee56fa
SolomiyaSmoliar <solomiia.smoliar@ucu.edu.ua>
2022-01-27 21:50:04 +0200

    Initial commit

---
+++ src/test/java/atm/ATMTest.java
@@ -0,0 +1,10 @@
+package atm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ATMTest {
+
+    @org.junit.jupiter.api.Test
+    void process() {
+    }
+}
---
+++ src/test/java/atm/DefaultCheckTest.java
@@ -0,0 +1,14 @@
+package atm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultCheckTest {
+
+    @org.junit.jupiter.api.Test
+    void setNext() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void process() {
+    }
+}
